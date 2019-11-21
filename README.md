65f38d320e4b0a5cbc93d7d6ece823a1a6cba107
DavidWells <hello@davidwells.io>
2019-11-20 17:06:24 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Netlify Plugins
+
+Netlify plugins are a new way to extend the functionality of your build on Netlify.
+
+For more information see the [@Netlify/build](https://github.com/netlify/build) project.
+
+## Community Plugins
+
+Below are plugins created by some awesome people! ❤️
+
+To add a plugin, add informations to the [plugins.json file]('./plugins.json').
+
+<!-- AUTO-GENERATED-CONTENT:START (GENERATE_PLUGIN_TABLE)-->
+| Plugin | Author |
+|:---------------------------|:-----------:|
+| **[Build Plugin Speedcurve - `netlify-build-plugin-speedcurve`](https://github.com/tkadlec/netlify-build-plugin-speedcurve)** <br/>  After a successful build, tell SpeedCurve you've deployed and trigger a round of testing | [tkadlec](http://github.com/tkadlec) |
+| **[Checklinks - `netlify-plugin-checklinks`](https://github.com/munter/netlify-plugin-checklinks)** <br/>  Checklinks helps you keep all your asset references correct and avoid embarrassing broken links to your internal pages, or even to external pages you link out to. | [munter](http://github.com/munter) |
+| **[Hashfiles - `netlify-plugin-hashfiles`](https://github.com/munter/netlify-plugin-hashfiles)** <br/>  Hashfiles sets you up with an optimal caching strategy for static sites, where static assets across pages are cached for as long as possible in the visitors browser and never have to be re-requested. | [munter](http://github.com/munter) |
+| **[Hours Plugin - `netlify-plugin-hours-plugin`](https://github.com/neverendingqs/netlify-deployment-hours-plugin)** <br/>  A Netlify build plugin that blocks deployment if it outside of deployment hours. | [neverendingqs](http://github.com/neverendingqs) |
+| **[Subfont - `netlify-plugin-subfont`](https://github.com/munter/netlify-plugin-subfont)** <br/>  Subfont post-processes your web page to analyse you usage of web fonts, then reworks your webpage to use an optimal font loading strategy for the best performance. | [munter](http://github.com/munter) |
+<!-- AUTO-GENERATED-CONTENT:END -->
+
+To add a plugin, add informations to the [plugins.json file]('./plugins.json').
---
+++ scripts/docs.js
@@ -0,0 +1,73 @@
+/**
+ * This example generated adds content to the repos README.md file
+ */
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+const markdownMagic = require('markdown-magic')
+
+const commonPartRe = /(?:(?:^|-)netlify-plugin(?:-|$))|(?:(?:^|-)netlify(?:-|$))/
+
+const config = {
+  transforms: {
+    /*
+    In readme.md the below comment block adds the list to the readme
+    <!-- AUTO-GENERATED-CONTENT:START (GENERATE_PLUGIN_TABLE)-->
+      plugin list will be generated here
+    <!-- AUTO-GENERATED-CONTENT:END -->
+     */
+    GENERATE_PLUGIN_TABLE: function(content, options) {
+      const commandsFile = path.join(__dirname, '..', 'plugins.json')
+      const plugins = JSON.parse(fs.readFileSync(commandsFile, 'utf8'))
+      let md = '| Plugin | Author |\n'
+      md += '|:---------------------------|:-----------:|\n'
+
+      plugins.sort(function (a, b) {
+        const aName = a.name.toLowerCase()
+        const bName = b.name.toLowerCase()
+        return aName.replace(commonPartRe, '').localeCompare(bName.replace(commonPartRe, '')) ||
+          aName.localeCompare(bName)
+      }).forEach(function(data) {
+        const userName = username(data.repo)
+        const profileURL = `http://github.com/${userName}`
+        const repoName = data.repo.split('.com/')[1]
+        md += `| **[${formatPluginName(data.name)} - \`${data.name.toLowerCase()}\`](${data.repo})** <br/> `
+        md += ` ${data.description} | `
+        md += `[${userName}](${profileURL}) |\n`
+      })
+      return md.replace(/^\s+|\s+$/g, '')
+    }
+  }
+}
+
+function username(repo) {
+  if (!repo) {
+    return null
+  }
+
+  var o = url.parse(repo)
+  var path = o.path
+
+  if (path.length && path.charAt(0) === '/') {
+    path = path.slice(1)
+  }
+
+  path = path.split('/')[0]
+  return path
+}
+
+function formatPluginName (string) {
+  return toTitleCase(string.toLowerCase().replace(commonPartRe, '').replace(/-/g, ' '))
+}
+
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  }
+  )
+}
+
+const markdownPath = path.join(__dirname, '..', 'README.md')
+markdownMagic(markdownPath, config, function() {
+  console.log('Docs updated!')
+})
