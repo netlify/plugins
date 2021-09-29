302fc234a067b9d53dd53892d94a1630affcad06
ehmicky <ehmicky@gmail.com>
2021-09-29 22:23:29 +0200

    chore: simplify linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,5 @@ const { overrides } = require('@netlify/eslint-config-node')
 
 module.exports = {
   extends: '@netlify/eslint-config-node',
-  overrides: [
-    ...overrides,
-    {
-      files: ['*.mjs'],
-      parserOptions: {
-        sourceType: 'module',
-      },
-      rules: {
-        'import/extensions': [2, 'always'],
-      },
-    },
-  ],
+  overrides: [...overrides],
 }
