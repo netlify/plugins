b50a15e09606f36d4ed8802527a4cd8697ec0cac
ehmicky <ehmicky@gmail.com>
2022-01-10 19:56:46 +0100

    fix: fix linting error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 
-const PLUGINS_FILE = fileURLToPath(new URL('./site/plugins.json', import.meta.url))
+const PLUGINS_FILE = fileURLToPath(new URL('site/plugins.json', import.meta.url))
 
 // TODO: replace with a JSON import once this is supported without any
 // experimental flag
