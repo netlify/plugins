900a6aa86e417a0fb7cfbc35a5b15844e3854239
Sean Roberts <sean.roberts@netlify.com>
2022-12-13 16:23:13 -0500

    feat: update FPP with WFUI

--- bin/publish_workflow_ui.sh
+++ bin/publish_workflow_ui.sh
@@ -16,6 +16,8 @@ do
 
   if [ -f package/workflow-ui.json ]; then
     echo "Copying workflow-ui.json for $PACKAGE"
+
+    mkdir -p site/$PACKAGE
     cp package/workflow-ui.json site/$PACKAGE/workflow-ui.json
 
     cat site/$PACKAGE/workflow-ui.json | jq ".surfaces | .[] | .surfaceScripts | .[]" | while read SCRIPT
@@ -39,9 +41,9 @@ git add site
 # See if we have any changes. We should.
 if [[ -n "$(git status --porcelain)" ]]; then
   echo "Creating PR \"$PR_TITLE\" for branch $BRANCH_NAME"
-  git commit -m "$PR_TITLE"
-  git push origin $BRANCH_NAME
-  gh pr create --title "$PR_TITLE" --body "This is an automated PR to publish workflow-ui" --label "workflow_ui" --label "automerge"
+  # git commit -m "$PR_TITLE"
+  # git push origin $BRANCH_NAME
+  # gh pr create --title "$PR_TITLE" --body "This is an automated PR to publish workflow-ui" --label "workflow_ui" --label "automerge"
 else
   # Shouldn't end up here, but log that there was nothing to sync
   echo "Looks like there was nothing to sync."
