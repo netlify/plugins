feb71db519fc4e3f488a9788201f0cdd506bfaa7
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-12-06 18:11:02 +0100

    [tmp] Update pluginsUrl to point to deploy preview

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const PLUGINS_FILE = fileURLToPath(new URL('site/plugins.json', import.meta.url)
 // TODO: replace with a JSON import once this is supported without any
 // experimental flag
 export const pluginsList = JSON.parse(readFileSync(PLUGINS_FILE))
-export const pluginsUrl = 'https://list-v2--netlify-plugins.netlify.app/plugins.json'
+export const pluginsUrl = 'https://deploy-preview-1450--netlify-plugins.netlify.app/plugins.json'
